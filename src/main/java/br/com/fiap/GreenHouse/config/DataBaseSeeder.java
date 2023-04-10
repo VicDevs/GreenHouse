064ca91fb6f1822321689a67396180a81f18455a
VicDevs <Victorhugo.fjm@gmail.com>
2023-04-10 03:25:01 -0300

    DataBaseSeeder

---
+++ src/main/java/br/com/fiap/GreenHouse/config/DataBaseSeeder.java
@@ -0,0 +1,14 @@
+package br.com.fiap.GreenHouse.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Não sei o que deixar pronto!
+    }
+        
+}
